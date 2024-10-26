badbfcc0bbbcc1bd35c5dd63ef7e337a589b738a
Logan Alexander <loganpalexander@gmail.com>
2024-10-26 15:25:02 +0200

    Getting tailwind up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mechanical-portfolio';
+  todayDate = new Date();  // Initialize the current date
 }
