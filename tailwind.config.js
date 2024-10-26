b1d59204d396cc1f8bb0bc58dea887dcae67fc63
Logan Alexander <loganpalexander@gmail.com>
2024-10-26 15:35:02 +0200

    adding basic drawing borders

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
-  ],
+  "./src/**/*.{html,ts}",
+],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '1/8': '12.5%', // For the grid markers
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
+
