472c428a3bb7bf539c123c8f37b52c3de3dd4c87
Logan Alexander <loganpalexander@gmail.com>
2025-01-13 15:20:43 -0800

    Adding wiki linked to obsidian publish site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { WikiComponent } from './wiki/wiki.component';
+// import { WikiComponent } from './wiki/wiki.component';
 import { ThreeModelComponent } from './three-model/three-model.component';
 
 export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'portfolio', component: PortfolioComponent },
-  { path: 'wiki', component: WikiComponent },
+  // { path: 'wiki', component: WikiComponent },
   { path: 'threemodel', component: ThreeModelComponent },
   { path: '', redirectTo: '/threemodel', pathMatch: 'full' },  // Default route
 ];
